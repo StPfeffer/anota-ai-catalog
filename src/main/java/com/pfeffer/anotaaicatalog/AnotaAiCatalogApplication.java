201de34fb1195dcca56160fdefc0bcf4bb5e262a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 20:33:01 -0300

    Initial commit

---
+++ src/main/java/com/pfeffer/anotaaicatalog/AnotaAiCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.pfeffer.anotaaicatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaAiCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfeffer/anotaaicatalog/AnotaAiCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfeffer.anotaaicatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
