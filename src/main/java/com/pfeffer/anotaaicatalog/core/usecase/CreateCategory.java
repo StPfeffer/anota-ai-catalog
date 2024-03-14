7ffb650fa5475bff4bc2569fd7501be13d8cb607
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:48:15 -0300

    Alteração nos usecases para serem interfaces

--- src/main/java/com/pfeffer/anotaaicatalog/core/usecase/CreateCategory.java
+++ src/main/java/com/pfeffer/anotaaicatalog/core/usecase/CreateCategory.java
@@ -1,22 +1,9 @@
 package com.pfeffer.anotaaicatalog.core.usecase;
 
 import com.pfeffer.anotaaicatalog.core.dto.CategoryDTO;
-import com.pfeffer.anotaaicatalog.core.entity.CategoryBO;
-import com.pfeffer.anotaaicatalog.core.mapper.CategoryMapper;
-import com.pfeffer.anotaaicatalog.core.repository.ICategoryDataBaseRepository;
 
-public class CreateCategory {
+public interface CreateCategory {
 
-    private final ICategoryDataBaseRepository repository;
-
-    public CreateCategory(ICategoryDataBaseRepository repository) {
-        this.repository = repository;
-    }
-
-    public CategoryDTO execute(CategoryDTO dto) {
-        CategoryBO entity = repository.persist(CategoryMapper.toBO(dto));
-
-        return CategoryMapper.toDTO(entity);
-    }
+    CategoryDTO create(CategoryDTO dto);
 
 }
