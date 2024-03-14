d40cc51f56b26ccd87cd87fd6046f3631b0e6572
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:23:37 -0300

    Implementação do ProductMapper

---
+++ src/main/java/com/pfeffer/anotaaicatalog/core/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.pfeffer.anotaaicatalog.core.mapper;
+
+import com.pfeffer.anotaaicatalog.core.dto.ProductDTO;
+import com.pfeffer.anotaaicatalog.core.entity.ProductBO;
+
+public class ProductMapper {
+
+    public static ProductDTO toDTO(ProductBO bo) {
+        ProductDTO dto = new ProductDTO();
+
+        dto.setId(bo.getId());
+        dto.setDescription(bo.getDescription());
+        dto.setPrice(bo.getPrice());
+        dto.setCategoryId(bo.getCategoryId());
+        dto.setOwnerId(bo.getOwnerId());
+
+        return dto;
+    }
+
+    public static ProductBO toBO(ProductDTO dto) {
+        ProductBO bo = new ProductBO();
+
+        bo.setId(dto.getId());
+        bo.setDescription(dto.getDescription());
+        bo.setPrice(dto.getPrice());
+        bo.setCategoryId(dto.getCategoryId());
+        bo.setOwnerId(dto.getOwnerId());
+
+        return bo;
+    }
+
+}
