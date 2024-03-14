d0ab9c7867bd9d197e5a520587ae85da19e3cdb5
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:25:41 -0300

    Criação do document Product

---
+++ src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/model/Product.java
@@ -0,0 +1,30 @@
+package com.pfeffer.anotaaicatalog.infra.mongo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String categoryId;
+
+    private String ownerId;
+
+}
