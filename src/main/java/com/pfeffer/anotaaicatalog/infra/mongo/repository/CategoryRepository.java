e8e46f0637f007fb8efa08db7fd563356578b649
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:37:14 -0300

    Ajuste no CategoryRepository para extender o ICategoryDataBaseRepository

--- src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/repository/CategoryRepository.java
+++ src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.pfeffer.anotaaicatalog.infra.mongo.repository;
 
+import com.pfeffer.anotaaicatalog.core.repository.ICategoryDataBaseRepository;
 import com.pfeffer.anotaaicatalog.infra.mongo.model.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends MongoRepository<Category, String> {
+public interface CategoryRepository extends MongoRepository<Category, String>, ICategoryDataBaseRepository {
 }
