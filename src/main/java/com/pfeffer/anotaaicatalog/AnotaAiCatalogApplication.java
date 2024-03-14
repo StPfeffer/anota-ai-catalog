607ba3608963f9d84aeddfae5759466c05fcd659
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:06:52 -0300

    Adicionada anotação para habilitar os repositórios Mongo

--- src/main/java/com/pfeffer/anotaaicatalog/AnotaAiCatalogApplication.java
+++ src/main/java/com/pfeffer/anotaaicatalog/AnotaAiCatalogApplication.java
@@ -2,8 +2,10 @@ package com.pfeffer.anotaaicatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AnotaAiCatalogApplication {
 
 	public static void main(String[] args) {
