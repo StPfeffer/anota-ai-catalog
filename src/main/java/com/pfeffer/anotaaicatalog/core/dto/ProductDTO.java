be834481247af184148a6b505490f08b5414a872
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:23:27 -0300

    Criação do ProductDTO

---
+++ src/main/java/com/pfeffer/anotaaicatalog/core/dto/ProductDTO.java
@@ -0,0 +1,57 @@
+package com.pfeffer.anotaaicatalog.core.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDTO {
+
+    private String id;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String categoryId;
+
+    private String ownerId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+}
