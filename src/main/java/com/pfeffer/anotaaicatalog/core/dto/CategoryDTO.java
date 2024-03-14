be2b33c00d8eeb95c0a7d4e7e4524fbcd95e903a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:12:10 -0300

    Criação do CategoryDTO

---
+++ src/main/java/com/pfeffer/anotaaicatalog/core/dto/CategoryDTO.java
@@ -0,0 +1,35 @@
+package com.pfeffer.anotaaicatalog.core.dto;
+
+public class CategoryDTO {
+
+    private String title;
+
+    private String ownerId;
+
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
