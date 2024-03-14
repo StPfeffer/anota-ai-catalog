669e0ff57353caa5d69a929806746dbadce901ee
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:37:05 -0300

    Ajuste no ProductRepository para extender o IProductDataBaseRepository

--- src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/repository/ProductRepository.java
+++ src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.pfeffer.anotaaicatalog.infra.mongo.repository;
 
+import com.pfeffer.anotaaicatalog.core.repository.IProductDataBaseRepository;
 import com.pfeffer.anotaaicatalog.infra.mongo.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String>, IProductDataBaseRepository {
 }
