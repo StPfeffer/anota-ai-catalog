58616d3adac8c238b273cd46974698ba0e541ae1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:19:51 -0300

    Adicionada propriedade id ao CategoryDTO

--- src/main/java/com/pfeffer/anotaaicatalog/core/dto/CategoryDTO.java
+++ src/main/java/com/pfeffer/anotaaicatalog/core/dto/CategoryDTO.java
@@ -2,11 +2,21 @@ package com.pfeffer.anotaaicatalog.core.dto;
 
 public class CategoryDTO {
 
+    private String id;
+
     private String title;
 
     private String ownerId;
 
     private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
