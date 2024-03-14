7b4a7c93745ee31150344cd9578a583b37a2f228
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 22:01:23 -0300

    Implementação dos usecases do FindCategory no CategoryService

--- src/main/java/com/pfeffer/anotaaicatalog/services/CategoryService.java
+++ src/main/java/com/pfeffer/anotaaicatalog/services/CategoryService.java
@@ -2,14 +2,17 @@ package com.pfeffer.anotaaicatalog.services;
 
 import com.pfeffer.anotaaicatalog.core.dto.CategoryDTO;
 import com.pfeffer.anotaaicatalog.core.mapper.CategoryMapper;
-import com.pfeffer.anotaaicatalog.core.usecase.CreateCategory;
+import com.pfeffer.anotaaicatalog.core.usecase.category.CreateCategory;
+import com.pfeffer.anotaaicatalog.core.usecase.category.FindCategory;
 import com.pfeffer.anotaaicatalog.infra.mongo.mapper.MongoCategoryMapper;
 import com.pfeffer.anotaaicatalog.infra.mongo.model.Category;
 import com.pfeffer.anotaaicatalog.infra.mongo.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class CategoryService implements CreateCategory {
+public class CategoryService implements CreateCategory, FindCategory {
 
     private final CategoryRepository repository;
 
@@ -24,6 +27,29 @@ public class CategoryService implements CreateCategory {
         this.repository.save(newCategory);
 
         return CategoryMapper.toDTO(MongoCategoryMapper.toDomain(newCategory));
+    }
+
+    @Override
+    public List<CategoryDTO> getAll() {
+        List<Category> categories = this.repository.findAll();
+
+        return categories.stream()
+                .map(MongoCategoryMapper::toDomain)
+                .toList()
+                .stream()
+                .map(CategoryMapper::toDTO)
+                .toList();
+    }
+
+    @Override
+    public CategoryDTO findCategory(String id) {
+        Category category = this.repository.findById(id).orElse(null);
+
+        if (category == null) {
+            return null;
+        }
+
+        return CategoryMapper.toDTO(MongoCategoryMapper.toDomain(category));
     }
 
 }
