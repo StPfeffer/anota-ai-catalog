4498eaee2e26bd60343a5ef9f620949c725ee416
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-13 21:19:54 -0300

    Adicionada propriedade id ao CategoryBO

--- src/main/java/com/pfeffer/anotaaicatalog/core/entity/CategoryBO.java
+++ src/main/java/com/pfeffer/anotaaicatalog/core/entity/CategoryBO.java
@@ -2,11 +2,21 @@ package com.pfeffer.anotaaicatalog.core.entity;
 
 public class CategoryBO {
 
+    private String id;
+
     private String title;
 
     private String ownerId;
 
     private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
