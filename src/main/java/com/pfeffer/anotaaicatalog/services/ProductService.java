b98afef8df125981ad4eda116d4cacf3b1a14c43
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-15 16:21:31 -0300

    Implementação do ProductService

--- src/main/java/com/pfeffer/anotaaicatalog/services/ProductService.java
+++ src/main/java/com/pfeffer/anotaaicatalog/services/ProductService.java
@@ -1,2 +1,34 @@
-package com.pfeffer.anotaaicatalog.services;public class ProductService {
+package com.pfeffer.anotaaicatalog.services;
+
+import com.pfeffer.anotaaicatalog.core.dto.ProductDTO;
+import com.pfeffer.anotaaicatalog.core.mapper.ProductMapper;
+import com.pfeffer.anotaaicatalog.core.usecase.CreateProduct;
+import com.pfeffer.anotaaicatalog.infra.mongo.mapper.MongoProductMapper;
+import com.pfeffer.anotaaicatalog.infra.mongo.model.Product;
+import com.pfeffer.anotaaicatalog.infra.mongo.repository.CategoryRepository;
+import com.pfeffer.anotaaicatalog.infra.mongo.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService implements CreateProduct {
+
+    private final ProductRepository repository;
+
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(ProductRepository repository, CategoryRepository categoryRepository) {
+        this.repository = repository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public ProductDTO create(ProductDTO dto) {
+        Product newProduct = new Product(dto);
+        newProduct.setCategory(categoryRepository.findById(dto.getCategoryId()).orElse(null));
+
+        this.repository.save(newProduct);
+
+        return ProductMapper.toDTO(MongoProductMapper.toDomain(newProduct));
+    }
+
 }
