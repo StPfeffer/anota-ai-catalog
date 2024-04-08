6da3525d592f27af7ab281569cddf0ec35558c3e
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-08 01:21:46 -0300

    Custom toString() para Category e Product

--- src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/model/Category.java
+++ src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/model/Category.java
@@ -4,6 +4,7 @@ import com.pfeffer.anotaaicatalog.core.category.CategoryDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -26,6 +27,19 @@ public class Category {
         this.title = dto.title();
         this.description = dto.description();
         this.ownerId = dto.ownerId();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+
+        json.put("id", this.id);
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("type", "category");
+
+        return json.toString();
     }
 
 }
--- src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/model/Product.java
+++ src/main/java/com/pfeffer/anotaaicatalog/infra/mongo/model/Product.java
@@ -4,6 +4,7 @@ import com.pfeffer.anotaaicatalog.core.product.ProductDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -34,6 +35,21 @@ public class Product {
         this.price = dto.price();
         this.ownerId = dto.ownerId();
         this.categoryId = dto.categoryId();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+
+        json.put("id", this.id);
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("price", this.price);
+        json.put("ownerId", this.ownerId);
+        json.put("categoryId", this.categoryId);
+        json.put("type", "product");
+
+        return json.toString();
     }
 
 }
